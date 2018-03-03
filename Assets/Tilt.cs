1c9cd4e57392853fcb4c8f0a91a000d6d400ebbb
Alex Lara <alexlara_97@hotmail.com>
2018-03-03 17:26:16 -0600

    changed colliders

---
+++ Assets/Tilt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour {
+
+    void Update(){
+        if (Input.GetKey("left"))
+        {
+			transform.Rotate(Vector3.back);
+        }
+        if (Input.GetKey("right"))
+        {
+			transform.Rotate(Vector3.forward);
+        }
+    }
+}
